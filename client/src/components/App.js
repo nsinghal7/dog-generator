b55e3690a7eeee3be4f54b5554336b94cdd5e75b
Kye Burchard <kyeb@mit.edu>
2021-01-10 14:09:56 -0500

    convert app.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,47 +5,37 @@ import DogViewer from "./DogViewer";
 import "./App.css";
 import "../utilities.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      breed: "pembroke",
-      iteration: 0
-    };
-  }
+const { useState } = React;
 
-  onKeyDown = event => {
+const App = () => {
+  const [breed, setBreed] = useState("pembroke");
+  const [iteration, setIteration] = useState(0);
+
+  const onKeyDown = (event) => {
     if (event.keyCode === 13) {
       // 13 is the enter key
       if (event.target.value === "") {
         // refresh the current dog if you just hit enter
-        this.setState({
-          iteration: this.state.iteration + 1
-        });
+        setIteration(iteration + 1);
       } else {
         // load a new dog if you typed a breed
-        this.setState({
-          breed: event.target.value,
-          iteration: this.state.iteration + 1
-        });
+        setBreed(event.target.value);
+        setIteration(iteration + 1);
         event.target.value = "";
       }
     }
   };
 
-  render() {
-    const { breed, iteration } = this.state;
-    return (
-      <div className="u-flex u-flexColumn u-flex-alignCenter">
-        <label className="App-label" htmlFor="dog-breed">
-          enter dog breed:
-        </label>
-        <input onKeyDown={this.onKeyDown} id="dog-breed" autoComplete="off" />
-        <GoodBoiMeter breed={breed} />
-        <DogViewer breed={breed} iteration={iteration} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="u-flex u-flexColumn u-flex-alignCenter">
+      <label className="App-label" htmlFor="dog-breed">
+        enter dog breed:
+      </label>
+      <input onKeyDown={onKeyDown} id="dog-breed" autoComplete="off" />
+      <GoodBoiMeter breed={breed} />
+      <DogViewer breed={breed} iteration={iteration} />
+    </div>
+  );
+};
 
 export default App;
