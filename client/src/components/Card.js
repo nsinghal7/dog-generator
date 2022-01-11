132f5eea4414607406a70f966d4afd8610a46cfe
Nikhil Singhal <niks@mit.edu>
2022-01-11 04:04:46 -0500

    use hooks

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Card.css";
 
-export default (inputComponent) => {
-  return <div className="Card-wrapper">{inputComponent}</div>;
+export default (props) => {
+  return <div className="Card-wrapper">{props.contents}</div>;
 };
